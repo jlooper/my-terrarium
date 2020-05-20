41f7142d7807121344686c43892b77ce38b85909
jeffcooney <65251425+jeffcooney@users.noreply.github.com>
2020-05-20 10:26:18 -0400

    added changes based on video

--- script.js
+++ script.js
@@ -26,23 +26,32 @@ function dragElement(terrariumElement) {
 		pos4 = 0;
 
 	//1. when you touch the terrariumElement, start tracking the pointer
+	terrariumElement.onpointerdown = pointerdrag;
 
 	function pointerDrag(e) {
 		e.preventDefault();
 		console.log(e);
 		//2. set pos3 to be e's clientX
+		pos3 = e.clientX;
 		//3. set pos4 to be e's clientY
+		pos4 = e.clientY;
 		//4. when the mouse moves, start the drag
+		document.onpointermove = elementDrag;
 		//5. when the mouse is lifted, stop the drag
+		document.onpointerup = closeDragElement;
 	}
 
 	function elementDrag(e) {
 		// calculate the new cursor position
 		//5. set pos1 = where the Xmouse WAS - where it IS
+		pos1 = pos3 - e.clientX;
 		//6. set pos2 = where the Ymouse WAS - where it IS
+		pos2 = pos4 = e.clientY;
 		//7. reset pos3 to current location of Xmouse
+		pos3 = e.clientX;
 		//8. reset pos4 to current location of Ymouse
-		//console.log(pos1, pos2, pos3, pos4);
+		pos4 = e.clientY;
+		console.log(pos1, pos2, pos3, pos4);
 		// set the element's new position:
 		terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
 		terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
@@ -51,6 +60,8 @@ function dragElement(terrariumElement) {
 	function closeDragElement() {
 		// stop calculating when mouse is released
 		//9. reset the raised pointer to null
+		document.onpointerup = null;
 		//10. reset the moved pointer to null
+		document.onpointermove = null;
 	}
 }
